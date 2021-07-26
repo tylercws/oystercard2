47963c8e3ed2a6b2b382dba749e3165607903b65
Tyler <tylercws@outlook.com>
2021-07-26 15:14:03 +0100

    Added top_up method and test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,6 +3,10 @@ attr_reader :balance
 
   def initialize(balance=0)
     @balance = balance
-  end 
+  end
+
+  def top_up(amount)
+    @balance += amount
+  end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
   it 'should have a balance of 0' do
-    oystercard = Oystercard.new 
+    oystercard = Oystercard.new
     expect(oystercard.balance).to eq(0)
   end
-end 
+
+  it 'expect to top up' do
+    oystercard = Oystercard.new
+    expect(oystercard).to respond_to(:top_up).with(1).argument
+  end
+end
