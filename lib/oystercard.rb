3b0dfdbf044e54933be263b48de61b950517c78b
Tyler <tylercws@outlook.com>
2021-07-26 15:32:27 +0100

    06_maximum_balance completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,21 @@
 class Oystercard
 attr_reader :balance
 
+MAX_LIMIT = 90
+
   def initialize(balance=0)
     @balance = balance
   end
 
+
   def top_up(amount)
+    fail "Reached max limit of £90" if full?
     @balance += amount
   end
-  
+
+  private
+  def full?
+    @balance == MAX_LIMIT
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,8 +6,13 @@ describe Oystercard do
     expect(oystercard.balance).to eq(0)
   end
 
-  it 'expect to top up' do
-    oystercard = Oystercard.new
-    expect(oystercard).to respond_to(:top_up).with(1).argument
+  it "should top up oystercard" do
+    expect {subject.top_up(5)}.to change { subject.balance }.by(5)
   end
+
+  it "should throw error if =>£90" do
+    allow(subject).to receive(:full?).and_return true
+    expect{subject.top_up(91)}.to raise_error("Reached max limit of £90")
+  end
+
 end
