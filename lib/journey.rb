7427cbe01f2a243d2e6856357bb25f0f6b43d29d
manu <manuelmoualek@gmail.com>
2021-07-29 15:56:16 +0100

    Added method to calculate fare + test

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,11 +1,27 @@
 class Journey
   def initialize()
-
-
+    @entry_station
+    @exit_station  
   end
 
   def begin_journey(entry_station,zone)
     @entry_station = Station.new(entry_station,zone)
     @entry_station.station_array
   end
+
+  def end_journey(exit_station, zone)
+    @exit_station = Station.new(exit_station,zone)
+    # @entry_station = nil
+    @exit_station.station_array
+  end
+
+  def fare
+    if @entry_station != nil
+      Oystercard::MINIMUM_FARE
+    else
+      6 
+    end   
+  end  
+  
+  
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -7,11 +7,14 @@ describe Journey do
   end
 
   it 'ends a journey' do
-    
+    expect(subject.end_journey("Deansgate",2)).to eq(["Deansgate",2])
   end
 
   it 'calculating fare' do
-    
+    journey = Journey.new
+    journey.begin_journey("Cornbrook",1)
+    journey.end_journey("Deansgate",2)
+    expect(journey.fare).to eq (1)
   end
 
   it 'know if a journey is complete' do
